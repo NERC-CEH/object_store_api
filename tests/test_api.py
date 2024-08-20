b3d4c9c80c9d874c44f8eb5661d773957a6bbf71
Jo Walsh <jowals@ceh.ac.uk>
2024-08-20 10:43:12 +0100

    smallest possible FastAPI test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,2 +1,15 @@
+from os_api.api import app
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
 def test_import_api_module():
     import os_api.api
+
